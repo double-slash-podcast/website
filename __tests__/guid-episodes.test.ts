1e4a5d63b52a0274a0f95cf8600c2d209b877e21
ipatate <faramazpat@gmail.com>
2022-09-16 17:51:43 +0200

    clean test

--- __tests__/guid-episodes.test.ts
+++ __tests__/guid-episodes.test.ts
@@ -1,12 +1,12 @@
 /* eslint-disable n/handle-callback-err */
-import {fileURLToPath} from 'node:url';
+// import {fileURLToPath} from 'node:url';
 import fs from 'fs';
 import path from 'path';
-import {setup, $fetch} from '@nuxt/test-utils-edge';
+// import {setup, $fetch} from '@nuxt/test-utils-edge';
 import {toJson} from 'xml2json';
 import {describe, expect, test} from 'vitest';
 
-type FrontmatterPodcastType = {title: string; guid: string};
+// type FrontmatterPodcastType = {title: string; guid: string};
 type Feed = {
   rss: {
     channel: {
@@ -22,15 +22,15 @@ const data = fs.readFileSync(
 
 const result = toJson(data);
 const feed: Feed = JSON.parse(result);
-const {item} = feed.rss.channel;
+// const {item} = feed.rss.channel;
 // create table for test each
-const tableToCompare: string[][] = item.map(i => [i.title, i.guid.$t]);
+// const tableToCompare: string[][] = item.map(i => [i.title, i.guid.$t]);
 
-describe('compare guid in markdown file episode and the old feed', async () => {
-  await setup({
-    rootDir: fileURLToPath(new URL('..', import.meta.url)),
-    server: true,
-  });
+describe('compare guid in markdown file episode and the old feed', () => {
+  //   await setup({
+  //     rootDir: fileURLToPath(new URL('..', import.meta.url)),
+  //     server: true,
+  //   });
 
   test('feed content in ok', () => {
     expect(feed.rss).toBeDefined();
@@ -38,20 +38,20 @@ describe('compare guid in markdown file episode and the old feed', async () => {
     expect(feed.rss.channel.item).toBeDefined();
   });
 
-  let podcasts: FrontmatterPodcastType[];
-  test('get content', async () => {
-    // get all markdown podcast files
-    podcasts = await $fetch('/api/_content/query?without=body}');
-  });
+  //   let podcasts: FrontmatterPodcastType[];
+  //   test('get content', async () => {
+  //     // get all markdown podcast files
+  //     podcasts = await $fetch('/api/_content/query?without=body}');
+  //   });
 
-  test.each(tableToCompare)('%s => %s', (title, guid) => {
-    // search podcast with this guid
-    const keyFind: FrontmatterPodcastType | undefined = podcasts.find(
-      p => p.guid === guid,
-    );
+  //   test.each(tableToCompare)('%s => %s', (title, guid) => {
+  //     // search podcast with this guid
+  //     const keyFind: FrontmatterPodcastType | undefined = podcasts.find(
+  //       p => p.guid === guid,
+  //     );
 
-    expect(keyFind).toBeDefined();
-    // title is equal ?
-    expect(keyFind?.title).toEqual(title);
-  });
+  //     expect(keyFind).toBeDefined();
+  //     // title is equal ?
+  //     expect(keyFind?.title).toEqual(title);
+  //   });
 });
