9eca33df07a41461c7c02ac1b214ae768e14df8a
ipatate <faramazpat@gmail.com>
2022-09-21 17:48:13 +0200

    WIP player track

--- components/Player.vue
+++ components/Player.vue
@@ -2,19 +2,39 @@
   <audio ref="audioPlayerElement" preload="auto">
     <source :src="props.src" :type="type" />
   </audio>
-  <h2>
-    {{ $formatTime(detailDuration.hours) }}:{{
-      $formatTime(detailDuration.minutes)
-    }}:{{ $formatTime(detailDuration.seconds) }}
-  </h2>
-  <h2>
-    {{ $formatTime(detailCurrentTime.hours) }}:{{
-      $formatTime(detailCurrentTime.minutes)
-    }}:{{ $formatTime(detailCurrentTime.seconds) }}
-  </h2>
-  <button @click="toggle">
-    <span v-if="state.status === 'pause'">Play</span><span v-else>Pause</span>
-  </button>
+  <div class="flex w-full">
+    <div class="w-32 p-10">
+      <button @click="toggle">
+        <span v-if="state.status === 'pause'">Play</span
+        ><span v-else>Pause</span>
+      </button>
+    </div>
+    <div class="flex flex-wrap w-full">
+      <input
+        :value="state.currentPosition"
+        class="w-full focus:outline-none"
+        aria-label="Seek"
+        type="range"
+        step="0.01"
+        min="0"
+        max="100"
+        autocomplete="off"
+        role="slider"
+        @click.stop="() => true"
+        @change.stop="updateCurrentTime"
+      />
+      <strong>
+        {{ $formatTime(detailCurrentTime.hours) }}:{{
+          $formatTime(detailCurrentTime.minutes)
+        }}:{{ $formatTime(detailCurrentTime.seconds) }}
+      </strong>
+      <strong class="ml-10">
+        {{ $formatTime(detailDuration.hours) }}:{{
+          $formatTime(detailDuration.minutes)
+        }}:{{ $formatTime(detailDuration.seconds) }}
+      </strong>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -35,7 +55,14 @@ const state: {
   status: 'play' | 'pause';
   currentTime: number;
   playbackRate: number;
-} = reactive({duration: 0, status: 'pause', currentTime: 0, playbackRate: 1});
+  currentPosition: number;
+} = reactive({
+  duration: 0,
+  status: 'pause',
+  currentTime: 0,
+  playbackRate: 1,
+  currentPosition: 0,
+});
 
 onMounted(() => {
   if (audioPlayerElement.value) {
@@ -73,9 +100,23 @@ const detailDuration = computed(
 // get object for currentTime
 const detailCurrentTime = computed(
   (): typeDuration =>
-    state.duration
+    state.currentTime
       ? calculateTotalValue(state.currentTime)
       : {hours: 0, seconds: 0, minutes: 0},
+);
+
+watch(
+  () => state.currentTime,
+  () =>
+    (state.currentPosition = audioPlayerElement.value
+      ? parseInt(
+          (
+            (audioPlayerElement.value.currentTime /
+              audioPlayerElement.value.duration) *
+            100
+          ).toString(),
+        )
+      : 0),
 );
 
 /** play sound or pause */
@@ -88,4 +129,64 @@ const toggle = () => {
     state.status = 'pause';
   }
 };
+
+const updateCurrentTime = (event: Event) => {
+  const {currentTarget} = event;
+  if (!audioPlayerElement.value || !currentTarget) return;
+  const _currentTime =
+    (+(currentTarget as HTMLInputElement).value *
+      audioPlayerElement?.value.duration) /
+    100;
+  audioPlayerElement.value.currentTime = _currentTime;
+  state.currentTime = _currentTime;
+};
 </script>
+
+<style scoped>
+input[type='range'] {
+  @apply relative appearance-none overflow-hidden cursor-pointer h-3 border-t border-transparent rounded-lg mx-3;
+}
+
+::-webkit-slider-runnable-track {
+  @apply bg-trackPurple;
+}
+
+::-moz-range-track {
+  @apply h-1.5 bg-trackPurple;
+}
+
+::-webkit-slider-thumb {
+  @apply w-0 h-1.5 appearance-none border-2 border-transparent shadow-thumb rounded-lg;
+}
+
+::-moz-range-thumb {
+  @apply w-0 h-1.5 bg-trackPurple border-0 border-transparent shadow-thumb;
+  border-radius: 0 !important;
+  box-sizing: border-box;
+}
+
+::-ms-thumb {
+  @apply w-0 h-1.5 bg-trackPurple border-0 border-transparent;
+  box-sizing: border-box;
+}
+
+::-ms-track {
+  @apply h-1.5 bg-trackPurple text-transparent border-0;
+}
+
+::-ms-fill-lower {
+  @apply h-1.5 bg-yellowDs;
+}
+
+::-ms-ticks-after {
+  @apply hidden;
+}
+
+::-ms-ticks-before {
+  @apply hidden;
+}
+
+::-ms-tooltip {
+  @apply hidden;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,40 +5,44 @@ module.exports = {
     extend: {
       colors: {
         darkPurple: '#3C0C55',
-        yellowDs: '#FFE40F'
+        yellowDs: '#FFE40F',
+        trackPurple: '#a700ff',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
       fontFamily: {
-        brand: ['Mono45', 'Arial','sans-serif'],
-        headings: ['Dank', 'Arial','sans-serif'],
-        base: ['Inter', 'Arial','sans-serif']
+        brand: ['Mono45', 'Arial', 'sans-serif'],
+        headings: ['Dank', 'Arial', 'sans-serif'],
+        base: ['Inter', 'Arial', 'sans-serif'],
       },
       animation: {
-        'slideLeft1':'slideLeft1 40s linear infinite',
-        'slideLeft2':'slideLeft2 40s linear infinite',
-        'slideRight1':'slideRight1 40s linear infinite',
-        'slideRight2':'slideRight2 40s linear infinite',
+        slideLeft1: 'slideLeft1 40s linear infinite',
+        slideLeft2: 'slideLeft2 40s linear infinite',
+        slideRight1: 'slideRight1 40s linear infinite',
+        slideRight2: 'slideRight2 40s linear infinite',
       },
       keyframes: {
         slideLeft1: {
-          '0%':{ transform: 'translateX(0%)'},
-          '100%': { transform: 'translateX(-100%)' }
+          '0%': {transform: 'translateX(0%)'},
+          '100%': {transform: 'translateX(-100%)'},
         },
         slideLeft2: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0)' }
+          '0%': {transform: 'translateX(100%)'},
+          '100%': {transform: 'translateX(0)'},
         },
         slideRight1: {
-          '0%':{ transform: 'translateX(-100%)'},
-          '100%': { transform: 'translateX(0)' }
+          '0%': {transform: 'translateX(-100%)'},
+          '100%': {transform: 'translateX(0)'},
         },
         slideRight2: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(100%)' }
-        }
-      }
+          '0%': {transform: 'translateX(0)'},
+          '100%': {transform: 'translateX(100%)'},
+        },
+      },
+      boxShadow: {
+        thumb: '-100vw 0 0 100vw #ffe40f',
+      },
     },
   },
   plugins: [],
