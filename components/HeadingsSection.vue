62c7ec696db33759626c207f391096035796bfe5
Alex Duval <alexduval71@gmail.com>
2022-09-29 14:37:58 +0200

    Navbar (#21)
    * Add navbar
    
    * Add prose and Massive work on ONE episode page

--- components/HeadingsSection.vue
+++ components/HeadingsSection.vue
@@ -4,15 +4,20 @@ defineProps({
     type: String,
     required: true,
   },
-    dark: {
+  dark: {
     type: Boolean,
-    default: false
+    default: false,
   },
 });
 </script>
 <template>
   <div class="max-w-3xl m-auto my-8">
-    <h3 class="mb-4 text-2xl text-center text-white" :class="{'text-haiti':dark}">{{ title }}</h3>
+    <h3
+      class="mb-4 text-2xl text-center text-white"
+      :class="{'text-haiti': dark}"
+    >
+      {{ title }}
+    </h3>
     <slot />
   </div>
 </template>
---
+++ components/Navbar.vue
@@ -0,0 +1,30 @@
+<script setup>
+const links = [
+  {
+    title: 'podcasts',
+    url: '/podcasts',
+  },
+  {
+    title: 'contact',
+    url: '#',
+  },
+];
+</script>
+<template>
+  <div
+    class="absolute top-0 z-20 flex space-x-4 text-white uppercase bg-transparent left-[50%] -translate-x-[50%]"
+  >
+    <nuxt-link
+      to="/"
+      class="px-2 py-2 hover:bg-gray-200 sm:hover:bg-yellowDs sm:hover:text-darkPurple"
+      >HOME</nuxt-link
+    >
+    <nuxt-link
+      v-for="{title, url} in links"
+      :key="url"
+      :to="url"
+      class="px-2 py-2 hover:bg-gray-200 sm:hover:bg-yellowDs sm:hover:text-darkPurple"
+      >{{ title }}</nuxt-link
+    >
+  </div>
+</template>
