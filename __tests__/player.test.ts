579a7b946d30c012c9fcb8f8bc00d05947e9fea3
ipatate <faramazpat@gmail.com>
2022-10-01 10:59:14 +0200

    fix test

--- __tests__/player.test.ts
+++ __tests__/player.test.ts
@@ -1,14 +1,14 @@
 import {describe, expect, test} from 'vitest';
-import {calculateTotalValue} from '../helpers/player';
+import {useCalculateTotalValue} from '../composables/useCalculateTotalValue';
 
 describe('test duration detail', () => {
   test('get object with detailed duration 1:08:55', () => {
-    const t = calculateTotalValue(4135);
+    const t = useCalculateTotalValue(4135);
     expect(t).toMatchSnapshot();
   });
 
   test('get object with detailed duration 0:51:10', () => {
-    const t = calculateTotalValue(3070);
+    const t = useCalculateTotalValue(3070);
     expect(t).toMatchSnapshot();
   });
 });
