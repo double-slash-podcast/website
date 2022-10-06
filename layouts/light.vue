6e9163e970cde7cb9a2ca1ac7ce230381c236d14
xlanex6 <alexduval71@gmail.com>
2022-10-06 09:10:44 +0200

    Fix: Layout with nice color

--- layouts/light.vue
+++ layouts/light.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="min-h-screen bg-gradient-to-b from-purple-50 to-purple-100">
-    <slot />
+    <div class="pb-12">
+      <slot />
+    </div>
+    <div class="fixed bottom-0 left-0 right-0 bg-darkPurple">
+      <LazyWrapperPlayer />
+    </div>
   </div>
 </template>
+<script setup>
+import LazyWrapperPlayer from '~/components/Player/WrapperPlayer.vue';
+</script>
