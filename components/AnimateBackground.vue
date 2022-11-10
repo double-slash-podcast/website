8382bf863f14ad1aea98c914dd6e5e9b85f110fb
ipatate <faramazpat@gmail.com>
2022-11-10 13:21:52 +0000

    try settimeout

--- components/AnimateBackground.vue
+++ components/AnimateBackground.vue
@@ -3,12 +3,16 @@
     <div
       class="absolute top-0 bottom-0 left-0 right-0 w-full bg-gradient-radial from-[#7D459A] to-darkPurple"
     ></div>
-    <IconsRows
-      v-for="i in rowCount"
-      :key="i"
-      :is-first="i === 1"
-      class="opacity-20"
-    />
+    <ClientOnly>
+      <span v-if="showSVG">
+        <IconsRows
+          v-for="i in rowCount"
+          :key="i"
+          :is-first="i === 1"
+          class="opacity-20"
+        />
+      </span>
+    </ClientOnly>
     <div
       class="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-r from-darkPurple to-darkPurple via-transparent"
     ></div>
@@ -21,6 +25,10 @@ const props = defineProps({
     default: 300,
   },
 });
+
+const showSVG = ref(false);
+
+onMounted(() => setTimeout(() => (showSVG.value = true), 1000));
 
 const rowCount = computed(() => {
   return Math.round(props.height / 60);
