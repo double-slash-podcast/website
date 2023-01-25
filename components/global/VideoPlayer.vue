d32a0feb17b1ab59fe9e1193f67d9cf55cb0aafc
ipatate <faramazpat@gmail.com>
2023-01-25 17:44:02 +0100

    update dependencies

--- components/global/VideoPlayer.vue
+++ components/global/VideoPlayer.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import LiteYouTubeEmbed from 'vue-lite-youtube-embed';
-import 'vue-lite-youtube-embed/dist/style.css';
+import 'vue-lite-youtube-embed/style.css';
 
 defineProps<{
   videoLink: string;
