22a3c83fed072dbbf9b23b87b45b5a8f83571345
ipatate <faramazpat@gmail.com>
2026-09-19 18:29:19 +0200

    Refactor player component usage in layout and pages
    - Replaced <LazyWrapperPlayer> with <Player> in WrapperPlayer.vue for improved clarity.
    - Commented out <LazyWrapperPlayer> in default.vue and <LazySlashIcon> in index.vue for potential future use.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -102,7 +102,7 @@ useSchemaOrg([defineWebPage()]);
       </Suspense>
       <SocialList class="mb-28" />
     </main>
-    <LazySlashIcon
+    <!-- <LazySlashIcon
       ref="bigSlash"
       size="350"
       class="hidden md:block fixed top-[25%] md:left-[-10%] z-8 opacity-20"
@@ -113,6 +113,6 @@ useSchemaOrg([defineWebPage()]);
       size="200"
       class="hidden md:block fixed top-[10%] right-0 z-8 opacity-20"
       inside-class="fill-purple-800"
-    />
+    /> -->
   </div>
 </template>
