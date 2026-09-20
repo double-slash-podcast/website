0efa23f6e42af2661efd24e3f24d002c1f965a27
ipatate <faramazpat@gmail.com>
2026-09-20 09:16:11 +0200

    Refactor default layout to enable useHead and useSchemaOrg functions
    - Reintroduced the useHead and useSchemaOrg functions in default.vue for improved SEO and structured data.
    - Removed the ClientOnly wrapper around the SearchModal component for direct rendering.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -8,9 +8,7 @@
       </div>
     </div>
     <!-- <UpButton /> -->
-    <ClientOnly>
-      <SearchModal />
-    </ClientOnly>
+    
   </div>
 </template>
 
@@ -23,50 +21,50 @@ const {
   baseInfos: {siteUrl, titleDefault, twitterUrl},
 } = useAppConfig();
 
-// useHead({
-//   htmlAttrs: {lang: 'fr-FR'},
-//   link: [
-//     ...agentDiscoveryLinks.map(link => ({
-//       rel: link.rel,
-//       href: link.href,
-//       ...(link.type ? {type: link.type} : {}),
-//       ...(link.title ? {title: link.title} : {}),
-//     })),
-//     {
-//       key: 'canonical',
-//       rel: 'canonical',
-//       href: `${siteUrl}${path}`,
-//     },
-//     {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
-//     {
-//       rel: 'apple-touch-icon',
-//       sizes: '180x180',
-//       href: '/apple-touch-icon.png',
-//     },
-//     {
-//       rel: 'icon',
-//       type: 'image/png',
-//       sizes: '32x32',
-//       href: '/favicon-32x32.png',
-//     },
-//     {
-//       rel: 'icon',
-//       type: 'image/png',
-//       sizes: '16x16',
-//       href: '/favicon-16x16.png',
-//     },
-//     {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
-//   ],
-// });
-// useSchemaOrg([
-//   defineOrganization({
-//     name: 'Double Slash Dev',
-//     logo: `${siteUrl}/logo.png`,
-//     sameAs: [twitterUrl],
-//   }),
-//   defineWebSite({
-//     name: titleDefault,
-//   }),
-//   defineWebPage(),
-// ]);
+useHead({
+  htmlAttrs: {lang: 'fr-FR'},
+  link: [
+    ...agentDiscoveryLinks.map(link => ({
+      rel: link.rel,
+      href: link.href,
+      ...(link.type ? {type: link.type} : {}),
+      ...(link.title ? {title: link.title} : {}),
+    })),
+    {
+      key: 'canonical',
+      rel: 'canonical',
+      href: `${siteUrl}${path}`,
+    },
+    {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png',
+    },
+    {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
+  ],
+});
+useSchemaOrg([
+  defineOrganization({
+    name: 'Double Slash Dev',
+    logo: `${siteUrl}/logo.png`,
+    sameAs: [twitterUrl],
+  }),
+  defineWebSite({
+    name: titleDefault,
+  }),
+  defineWebPage(),
+]);
 </script>
