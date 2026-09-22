6a94a3ed5e7ccc40eaec65d1bb61390926b1f7ab
ipatate <faramazpat@gmail.com>
2026-09-22 07:07:06 +0200

    Replace SearchHost with LazySearchHost for improved performance and update mask-icon color in default layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,7 +9,7 @@
     </div>
     <UpButton />
     <ClientOnly>
-      <SearchHost />
+      <LazySearchHost />
     </ClientOnly>
   </div>
 </template>
@@ -71,7 +71,7 @@ useHead({
       sizes: '16x16',
       href: '/favicon-16x16.png',
     },
-    {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
+    {rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#9123cb'},
   ],
 });
 useSchemaOrg([
